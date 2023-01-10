893757d5ee379d6a042463706c68fc165d7cc604
Seungho <hsh47607@naver.com>
2023-01-10 15:58:14 +0900

    create websocketconfig

---
+++ src/main/java/com/gaaji/chat/global/config/SpringWebSocketConfig.java
@@ -0,0 +1,11 @@
+package com.gaaji.chat.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class SpringWebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+}
